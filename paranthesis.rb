b051fbaed703dd83112d35ce6fdd3a8138b2618a
Hunor Kovács <hunor.kovacs@ft.com>
2015-12-17 17:49:41 +0200

    Day 5 2

--- paranthesis.rb
+++ paranthesis.rb
@@ -1,38 +1,40 @@
 require 'digest'
 
-def vowel(s)
-  s.count("aeiou") >= 3
-end
-
 def double(s)
   i = 0
   while i < s.length - 1 do
-   if (s[i] == s[i + 1])
-     return true
-   end
+    j = 0
+    while j < s.length - 1 do
+      if j + 1 < i || j > i + 1      
+        if (s[i..i + 1] == s[j..j + 1])
+          return true
+        end
+      end
+      j += 1
+    end
    i += 1
   end
   return false
 end
 
-def forbidden(s)
-  f = ["ab", "cd", "pq", "xy"]
-  f.each do |w|
-    unless s[w].nil?
-      return false
+def repeat(s)
+  i = 0
+  while i < s.length - 2 do
+    if (s[i] == s[i + 2])
+      return true
     end
+    i += 1
   end
-  return true
+  return false
 end
 
 File.open("paranthesis.txt", "r") do |f|
   nice = 0
   f.each_line do |line|
     l = line[0..line.length-2]
-    # puts "%s vowel %s" % [l, vowel(line)]
-    # puts "%s double %s" % [l, double(line)]
-    # puts "%s forbidden %s" % [l, forbidden(line)]
-    if vowel(l) && double(l) && forbidden(l)
+    puts "%s double %s" % [l, double(line)]
+    puts "%s repeat %s" % [l, repeat(line)]
+    if double(l) && repeat(l)
       nice += 1
     end
   end
