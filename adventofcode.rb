118095dadb3c578a7674fb3e9482e25464614bf8
Hunor Kovács <hunor.kovacs@ft.com>
2015-12-20 13:55:19 +0200

    Day 8 2

--- adventofcode.rb
+++ adventofcode.rb
@@ -1,10 +1,22 @@
+require 'yaml'
+
+def escape(s)
+  s.inspect
+end
+
 File.open("adventofcode-input.txt", "r") do |f|
   s = 0
+  ors = 0
+  es = 0
   f.each_line do |line|
     line = line.chomp
-    puts "%s|%s|%d|%d|%d" % [line, eval(line), line.length, eval(line).length, line.length - eval(line).length]
-    # puts "%d %d %d" % [line.length, eval(line).length, line.length - eval(line).length]
-    s += line.length - eval(line).length
+    puts "%s|%s|%d|%d|%d" % [line, escape(line), line.length, escape(line).length, escape(line).length - line.length]
+    ors += line.length
+    es += escape(line).length
+    s += escape(line).length - line.length
   end
+  puts ors
+  puts es
+  puts es - ors
   puts s
 end
