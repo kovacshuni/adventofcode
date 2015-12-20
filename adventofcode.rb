7828747d5b4d1e7253f093d323e54166efffbf3e
Hunor Kovács <hunor.kovacs@ft.com>
2015-12-20 15:26:37 +0200

    Day 9 1

--- adventofcode.rb
+++ adventofcode.rb
@@ -1,22 +1,47 @@
-require 'yaml'
+require 'set'
 
-def escape(s)
-  s.inspect
-end
+distances = Hash.new
+cities = Set.new
 
 File.open("adventofcode-input.txt", "r") do |f|
-  s = 0
-  ors = 0
-  es = 0
   f.each_line do |line|
     line = line.chomp
-    puts "%s|%s|%d|%d|%d" % [line, escape(line), line.length, escape(line).length, escape(line).length - line.length]
-    ors += line.length
-    es += escape(line).length
-    s += escape(line).length - line.length
+    matchdata = line.match(/(\w+) to (\w+) = (\d+)/)
+    # puts "%s - %s = %d" % [matchdata[1], matchdata[2], matchdata[3]]
+    cities << matchdata[1]
+    cities << matchdata[2]
+    ab = [ matchdata[1], matchdata[2] ]
+    ba = [ matchdata[2], matchdata[1] ]
+    distances[ab] = matchdata[3].to_i
+    distances[ba] = matchdata[3].to_i
   end
-  puts ors
-  puts es
-  puts es - ors
-  puts s
 end
+
+
+citiesa = cities.to_a
+mins = Array.new
+citiesa.permutation.to_a.each do |route|
+  s = 0
+  for i in 0..route.length - 2
+    s += distances[[route[i], route[i+1]]]
+  end
+  mins << s
+end
+
+puts mins.min
+
+# cities.each do |c1|
+#   min = 255
+#   started = false
+#   cities.each do |c2|
+#     unless started
+#       min = distances[[c1, c2]]
+#     end
+#     if c1 != c2
+#       if distances[[c1, c2]] < min
+#         min = distances[[c1, c2]]
+#       end
+#     end
+#   end
+#   puts "shortest from %s is %d" % [c1, min] 
+# end
